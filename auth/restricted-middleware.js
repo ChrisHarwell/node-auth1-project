cee97e5796963caab5d4ee4143753d8f2f37ac93
Chris Harwell <chrisharwell72@gmail.com>
2020-08-31 18:50:28 -0400

    MVP + Stretch Complete

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,8 @@
+module.exports = (req, res, next) => {
+    if(req.session && req.session.user) {
+        console.log(req.session)
+        next();
+    } else {
+        res.status(403).json({message: 'not authorized'});
+    }
+}
